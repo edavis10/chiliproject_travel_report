d63dc436f84899e500d3834597557cde43ee3955
Eric Davis <edavis@littlestreamsoftware.com>
2011-03-09 10:45:10 -0800

    Fix init.rb author

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ require 'redmine'
 
 Redmine::Plugin.register :redmine_travel_report do
   name 'Travel Report'
-  author 'Author name'
+  author 'Eric Davis'
   url 'https://projects.littlestreamsoftware.com/projects/redmine-misc'
   author_url 'http://www.littlestreamsoftware.com'
   description 'Shows travel requests'
